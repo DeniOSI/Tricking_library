2c77e60bfbbb2b556385276c53ae8c0756abb90b
Denis Osipenko <geksabyte@gmail.com>
2020-12-29 22:24:16 +0200

    VideoUpload

---
+++ TrickingLibrary.Api/Controllers/VideosController.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/videos")]
+    public class VideosController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHost;
+
+        public VideosController(IWebHostEnvironment webHost)
+        {
+            this._webHost = webHost;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadVideo(IFormFile video)
+        {
+            var mimeType = video.FileName.Split('.').Last();
+            var fileName = string.Concat(Path.GetRandomFileName(), '.', mimeType);
+            var filePath = string.Concat(_webHost.WebRootPath,'/', fileName);
+            await using var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            await video.CopyToAsync(fileStream);
+            return Ok();
+        }
+    }
+}
--- web-client/pages/index.vue
+++ web-client/pages/index.vue
@@ -1,21 +1,38 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo/>
-        <vuetify-logo/>
-      </div>   
-          {{ message }}
-          {{tricks}}
-          <v-btn @click="reset">Reset</v-btn>   
-    </v-col>
-  </v-row>
+
+  <div>
+
+    <v-file-input
+      label="File input"
+      outlined
+      dense
+      @change="handleFile"
+      accept = "video/*"
+    ></v-file-input>
+
+    {{ message }}
+    <v-btn @click="reset">Reset message</v-btn>
+
+    <div v-if="tricks">
+
+      <p v-for="t in tricks">
+        {{ t.name }}
+      </p>
+    </div>
+
+    <v-text-field v-model="trickName"></v-text-field>
+    <v-btn @click="saveTrick">Save trick</v-btn>
+
+    <v-btn @click="resetTricks">Reset tricks</v-btn>
+  </div>
+
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import {mapState, mapActions, mapMutations} from 'vuex'
+import Axios from "axios";
 
 export default {
   components: {
@@ -25,26 +42,36 @@ export default {
   data: () => ({
     trickName: ""
   }),
-  computed:{ 
+  computed: {
     ...mapState({
-    message: state => state.message}),
+      message: state => state.message
+    }),
     ...mapState('tricks', {
-    tricks: state => state.tricks})    
-    },
+      tricks: state => state.tricks
+    })
+  },
   methods: {
     ...mapMutations([
-    'reset'    
-  ]),
-  ...mapMutations({
-     resetTricks: 'reset'
-  }),
-  ...mapActions('tricks', ['createTrick']),
-  async saveTrick(){
-    await this.createTrick(this.trickName)
+      'reset'
+    ]),
+    ...mapMutations({
+      resetTricks: 'reset'
+    }),
+    ...mapActions('tricks', ['createTrick']),
+    async saveTrick() {
+      await this.createTrick(this.trickName)
+    },
+    async handleFile(file){
+      if(!file)
+      return;
+      console.log(file)
+        const result =  new FormData();
+        result.append("video", file)
+        await Axios.post('http://localhost:5000/api/videos', result);
+    }
   }
-  }
- 
- 
+
+
   // fetch: async function () {
   //   console.log("test fetchMessage")
   //   // await this.$store.dispatch("fetchMessage");
@@ -58,7 +85,6 @@ export default {
   //     })
   // }
 
- 
 
 }
 </script>
